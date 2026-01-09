f238fe08dfce72ccbd59cf92bfc6ae5a036fd837
Soumyadipta Seal <146237831+sseal2004@users.noreply.github.com>
2026-01-10 03:34:48 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (77.02%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,44 +10,19 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode temp1 = list1;
-        ListNode temp2 = list2;
-        //Initialize new head 
-        ListNode ans = null;
-        while(temp1!=null&&temp2!=null){
-            if(temp1.val<temp2.val){
-                ans = inserAtlast(ans,temp1.val);//smaller
-                temp1 = temp1.next;
-            }else{
-                ans = inserAtlast(ans,temp2.val);//smaller
-                temp2 = temp2.next;
+
+        if(list1!=null && list2!=null){
+        if(list1.val<list2.val){
+            list1.next=mergeTwoLists(list1.next,list2);
+            return list1;
             }
-            
+            else{
+                list2.next=mergeTwoLists(list1,list2.next);
+                return list2;
         }
-        //remaining element 
-            while(temp1!=null){
-                ans = inserAtlast(ans,temp1.val);
-                temp1 = temp1.next;
-            }
-            while(temp2!=null){
-                ans = inserAtlast(ans,temp2.val);
-                temp2 = temp2.next;
-
-            }
-            return ans;
-
-    }
-    static ListNode inserAtlast(ListNode head,int val){
-        
-        ListNode newNode = new ListNode(val);
-        if(head==null)return newNode;
-        ListNode ptr = head;
-
-        while(ptr.next!=null){
-            ptr = ptr.next;
         }
-        ptr.next=newNode;
-        newNode.next=null;
-        return head;
+        if(list1==null)
+            return list2;
+        return list1;
     }
 }
