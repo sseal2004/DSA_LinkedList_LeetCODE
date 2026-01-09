fe821fa8bb16f4bf7c042a73af543715e15e6a4a
Soumyadipta Seal <146237831+sseal2004@users.noreply.github.com>
2026-01-10 03:37:58 +0530

    Time: 1 ms (4.21%) | Memory: 44.4 MB (39.99%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,6 +10,50 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode temp1 = list1;
+        ListNode temp2 = list2;
+        //Initialize new head 
+        ListNode ans = null;
+        while(temp1!=null&&temp2!=null){
+            if(temp1.val<temp2.val){
+                ans = inserAtlast(ans,temp1.val);//smaller
+                temp1 = temp1.next;
+            }else{
+                ans = inserAtlast(ans,temp2.val);//smaller
+                temp2 = temp2.next;
+            }
+            
+        }
+        //remaining element 
+            while(temp1!=null){
+                ans = inserAtlast(ans,temp1.val);
+                temp1 = temp1.next;
+            }
+            while(temp2!=null){
+                ans = inserAtlast(ans,temp2.val);
+                temp2 = temp2.next;
+
+            }
+            return ans;
+
+    }
+    static ListNode inserAtlast(ListNode head,int val){
+        
+        ListNode newNode = new ListNode(val);
+        if(head==null)return newNode;
+        ListNode ptr = head;
+
+        while(ptr.next!=null){
+            ptr = ptr.next;
+        }
+        ptr.next=newNode;
+        newNode.next=null;
+        return head;
+    }
+}
+/*
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
         if(list1!=null && list2!=null){
         if(list1.val<list2.val){
@@ -26,3 +70,4 @@ class Solution {
         return list1;
     }
 }
+ */
