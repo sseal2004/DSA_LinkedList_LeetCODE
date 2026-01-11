5da2c837deb2730b68857160bb60eac8fba49fb9
Soumyadipta Seal <146237831+sseal2004@users.noreply.github.com>
2026-01-12 03:37:14 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (5.74%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -11,52 +11,22 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        int length=0;
-        ListNode fast =head;
+        if(head==null||head.next==null)return null;
+        ListNode fast = head;
         ListNode slow=head;
-        while(fast!=null&&fast.next!=null){
-         fast = fast.next.next;
-         slow = slow.next;
-         if(fast==slow) {
-            length= lengthOfCycle(slow);
-            break;
-         }
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast==slow){
+                slow=head;
+                while(slow!=fast){
+                    fast=fast.next;
+                    slow=slow.next;
+                }
+                return slow;
+            }
         }
-        //find the start
-        //take two pointer
-        if(length==0)return null;
-        ListNode first =head;
-        ListNode second = head;
-        while(length>0){
-            second = second.next;//move pointer second
-            length--;
-        }
-        //keeping moving both forward and they will meet at cycle start
-        while(first!=second){
-            first=first.next;
-            second=second.next;
-        }
-        return second;
+        return null;//default given
     }
-    static int lengthOfCycle(ListNode head){
-         ListNode fast =head;
-        ListNode slow=head;
-       while(fast!=null&&fast.next!=null){ 
-        fast=fast.next.next;
-        slow = slow.next;
-
-        ListNode temp= slow;
-        int length =0;
-        if(fast==slow){
-        do{
-            temp = temp.next;
-            length++;
-        }while(temp!=slow);
-
-
-        return length;
-        }
-       }
-       return 0;
-    }
+   
 }
