ce86cb0ae82c971fc978b8b60dfffb849892c0f5
Soumyadipta Seal <146237831+sseal2004@users.noreply.github.com>
2026-01-12 23:58:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (7.10%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -12,6 +12,7 @@ class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode f=head;
         ListNode s=head;
+        //if fast person move twice the slow person then slow person move moves half the fast person
         while(f!=null&&f.next!=null){
             f=f.next.next;
             s=s.next;
